83b46b7d11735e73ad24ed8449cc236ac50e7845
Hafiz Muhammad Salman <hafizmuhammadsalman13@gmail.com>
2026-07-12 19:10:04 -0700

    Create database.php

---
+++ Use AI _LangChain _ MISP_ to Enrich Raw Intel Feeds/app/Config/database.php
@@ -0,0 +1,10 @@
+public $default = array(
+    'datasource' => 'Database/Mysql',
+    'persistent' => false,
+    'host' => 'localhost',
+    'login' => 'misp',
+    'password' => 'misppassword',
+    'database' => 'misp',
+    'prefix' => '',
+    'encoding' => 'utf8',
+);
